4d94d5dd3c4e9b572488de815d0ba445e66d999e
kury <kury@kurybook.(none)>
2014-05-24 02:58:13 -0300

    Transição entre blocos OK

--- static/js/rede_javascript.js
+++ static/js/rede_javascript.js
@@ -19,18 +19,51 @@ document.addEventListener("DOMContentLoaded",main)
 
 function main(){
 
-	editando_perfil();
+	mudando_fase_perfil("1");
 
+	var btnPerfil = querySelectAll("data-irParaFase");
+	for (var i = 0; i < btnPerfil.length; i++) {
+		btnPerfil[i].addEventListener("click",function(){
+			mudando_fase_perfil(this.getAttribute("data-irParaFase"));
+		})
+	};
 }
 
-function editando_perfil(){
+function mudando_fase_perfil(fase){
+
 	var fases_edicao = querySelectAll("data-fase")
 	var status_fase = querySelectAll("data-fase-atual")
-	
+	var img_fase = querySelectAll("data-img-fase");
+
 	for (var i = 0; i < fases_edicao.length; i++) {
 		var status = fases_edicao[i].getAttribute("data-fase-atual");
+		// Identifica qual o bloco que vai
+		if (fases_edicao[i].getAttribute("data-fase") == fase){
+			status = "true";
+		}
+		else{
+			status = "false";
+		}
+
+		// Identifica quais as imagens da fase
+		for (var img = 0; img < img_fase.length; img++) {
+			if (img_fase[img].getAttribute("data-img-fase") == fase){
+				img_fase[img].style.display = "inline";
+			}
+			else
+			{
+				img_fase[img].style.display = "none";
+			}
+		};
+	
+		// Muda o bloco
 		if (status == "false"){
 			fases_edicao[i].style.display = "none";
-		};
+		}
+		else
+		{
+			fases_edicao[i].style.display = "block";	
+		}
+		;
 	};
 }
