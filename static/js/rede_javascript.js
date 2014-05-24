f50f835699829146470b17988605a2385ce87b89
kury <kury@kurybook.(none)>
2014-05-24 01:01:24 -0300

    Criação das fases de edição

--- static/js/rede_javascript.js
+++ static/js/rede_javascript.js
@@ -1,87 +1,36 @@
 // alert(screen.width)/Tamanho da Tela (do dispositivo)
 // window.innerWidth // Tamanho da Janela do navegador 
 
-function ID(elemento){
-	var elemento = document.getElementById(ID)
+// Função para pegar elemento pela ID
+// Retorna o Elemento
+function $(elemento){
+	var elemento = document.getElementById(elemento)
 	return elemento;
 }
+
+// Função para pegar basicamente elementos data-*
 function querySelectAll(parametro){
+	parametro = "["+parametro+"]";
 	var elementos = document.querySelectorAll(parametro);
 	return elementos;
 }
 
 document.addEventListener("DOMContentLoaded",main)
 
-window.addEventListener("resize",function(){
-	ajustaElementos(this.innerWidth);
-})
-
 function main(){
-	ajustaElementos(window.innerWidth);
-	console.log(document.querySelectorAll("data-conteudo"));
-}
 
-
-function ajustaElementos(larguraTela){
-
-	if(larguraTela >= 1200)
-	{
-		re_organiza("desktop");
-	}
-	else if(larguraTela >= 760 && larguraTela <1200)
-	{
-		re_organiza("tablet");
-	}
-	else if(larguraTela < 760){
-		elementosSmartphone();
-	}
+	editando_perfil();
 
 }
-function elementosDesktop(){};
-function elementosTablet(){};
-function elementosSmartphone(){};
 
-function re_organiza(tela){
-
-	var secoes = document.getElementsByTagName("section");
-	for (var i = 0; i < secoes.length; i++) {
-		var data = secoes[i].getAttribute("data-conteudo");
-		if (data != null){
-			
-			if (tela == "desktop"){
-				if (data == "descricao"){
-					secoes[i].className = secoes[i].className.replace("span_12","span_6")
-				};
-				if (data == "login"){
-					secoes[i].className = secoes[i].className.replace("span_12","span_6")
-				};
-				if (data == "registro"){
-					secoes[i].className = secoes[i].className.replace("span_6","span_5")
-				};
-				if (data == "login-desktop"){
-					secoes[i].style.display = "block"
-				};
-				console.log(data);
-			};
-
-
-			if (tela == "tablet"){
-				if (data == "descricao"){
-					secoes[i].className = secoes[i].className.replace("span_6","span_12")
-				};
-				if (data == "login"){
-					secoes[i].className = secoes[i].className.replace("span_6","span_12")
-				};
-				if (data == "registro"){
-					secoes[i].className = secoes[i].className.replace("span_5","span_6")
-				};
-				if (data == "login-desktop"){
-					secoes[i].style.display = "none"
-				};
-				console.log(data);
-			};
-
-			
-		}
+function editando_perfil(){
+	var fases_edicao = querySelectAll("data-fase")
+	var status_fase = querySelectAll("data-fase-atual")
+	
+	for (var i = 0; i < fases_edicao.length; i++) {
+		var status = fases_edicao[i].getAttribute("data-fase-atual");
+		if (status == "false"){
+			fases_edicao[i].style.display = "none";
+		};
 	};
 }
